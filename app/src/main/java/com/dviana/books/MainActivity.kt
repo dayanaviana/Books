66f27c1f1bf9bdcc6047d68be5937bc3a36b799b
dviana <dayanatsv@gmail.com>
2025-11-09 21:55:50 -0700

    add resources (image, drawable-night, color and strings with locale)

--- app/src/main/java/com/dviana/books/MainActivity.kt
+++ app/src/main/java/com/dviana/books/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.dviana.books.ui.theme.BooksTheme
 
@@ -18,26 +23,32 @@ class MainActivity : ComponentActivity() {
         super.onStart()
         println("onStart")
     }
+
     override fun onRestart() {
         super.onRestart()
         println("onRestart")
     }
+
     override fun onResume() {
         super.onResume()
         println("onResume")
     }
+
     override fun onPause() {
         super.onPause()
         println("onPause")
     }
+
     override fun onStop() {
         super.onStop()
         println("onStop")
     }
+
     override fun onDestroy() {
         super.onDestroy()
         println("onDestroy")
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -56,10 +67,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = stringResource(R.string.greeting) + " $name!",
+            modifier = modifier
+        )
+        colorResource(R.color.purple_200)
+        Image(
+            painter = painterResource(id = R.drawable.img_book_jacket),
+            contentDescription = null,
+        )
+    }
 }
 
 @Preview(showBackground = true)
